a4877925465485cf17e670000019d45bbe63bf79
amiedawn <amie.d.carroll@gmail.com>
2021-01-06 00:36:17 -0700

    app done

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -17,6 +18,9 @@ User.hasMany(Vote, {
   foreignKey: "user_id",
 });
 
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
@@ -32,6 +36,10 @@ Post.hasMany(Vote, {
   foreignKey: "post_id",
 });
 
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
 Vote.belongsTo(User, {
   foreignKey: "user_id",
 });
@@ -40,4 +48,12 @@ Vote.belongsTo(Post, {
   foreignKey: "post_id",
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Vote, Comment };
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Post, Vote } = require("../../models");
+const { User, Post, Comment, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
@@ -25,11 +25,19 @@ router.get("/:id", (req, res) => {
         attributes: ["id", "title", "post_url", "created_at"],
       },
       {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+      {
         model: Post,
-        attributes: ['title'],
+        attributes: ["title"],
         through: Vote,
-        as: 'voted_posts'
-      }
+        as: "voted_posts",
+      },
     ],
   })
     .then((dbUserData) => {
